36c917e4456423807262f69642bb6cb91784b118
prince2004patel <princepatel30324@gmail.com>
2025-01-01 18:03:26 +0530

    diseases and symptoms done

--- ml-backend/app.py
+++ ml-backend/app.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 import numpy as np
 import pandas as pd
 
-from src.diabetes.predict_pipeline import PredictPipeline, CustomData
+from src.diabetes.predict_pipeline import PredictPipeline as DiabetesPredictPipeline, CustomData as DiabetesCustomData
+from src.diseases_and_symptoms.predict_pipeline import PredictPipeline as DiseasePredictPipeline
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests
 
-@app.route('/predict-diabetes',methods=['POST'])
+# Route for predicting diabetes
+@app.route('/predict-diabetes', methods=['POST'])
 def predict_diabetes():
     try:
         data = request.get_json()
@@ -22,14 +24,37 @@ def predict_diabetes():
             if field not in data:
                 return jsonify({"error": f"Missing field: {field}"}), 400
             
-        custom_data = CustomData(**data)
+        custom_data = DiabetesCustomData(**data)
         input_df = custom_data.get_data_as_data_frame()
-        pipe = PredictPipeline()
+        pipe = DiabetesPredictPipeline()
         preds = pipe.predict(input_df)
 
         return jsonify({"prediction": preds.tolist()})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
+# Route for predicting diseases based on symptoms
+@app.route('/predict-disease-using-symptoms', methods=['POST'])
+def predict_disease():
+    try:
+        data = request.get_json()
+
+        # Validate input data
+        if 'symptoms' not in data:
+            return jsonify({"error": "Missing field: symptoms"}), 400
+        
+        symptoms_list = data['symptoms']
+
+        # Validate the number of symptoms
+        if not isinstance(symptoms_list, list) or len(symptoms_list) < 3 or len(symptoms_list) > 5:
+            return jsonify({"error": "Provide between 3 and 5 symptoms."}), 400
+
+        pipe = DiseasePredictPipeline()
+        predicted_disease = pipe.predict(symptoms_list)
+
+        return jsonify({"prediction": predicted_disease})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
