922fe6b0f24264f51c6d228253e11ba9059b4668
Patel purv <patelpurv2643@gmail.com>
2025-01-11 16:21:43 +0530

    Appointment page added

--- frontend/src/components/Templets/Appointment.jsx
+++ frontend/src/components/Templets/Appointment.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
 import Sidenav from '../../components/Sidenav'
+import AppointmentViewForDoctor from './AppointmentViewForDoctor'
+import AppointmentViewForPatient from './AppointmentViewForPatient'
+import './AppointmentView.css'
 const Appointment = () => {
   const navigate=useNavigate();
   return (
     <div className='w-screen h-screen bg-zinc-300 flex'>
-     {/* <div className='px-3 w-full h-[8vh] bg-[#212529] text-zinc-300 font-semibold flex items-center text-xl'>
-          <h1 > <i onClick={()=>navigate(-1)} className="hover:text-[#0077b6]  font-normal ri-arrow-left-line"></i> Appointments</h1>   
-     </div> */}
+     
      <div className='w-[25%] '>
    <Sidenav/>
    </div>
-   <div className='w-[75%] bg-blue-700 h-full text-center text-3xl'>
-   <h1 className='text-black'>Appointment section</h1>
+
+
+   <div className='w-[75%] bg-white-700 h-full text-center text-3xl'>
+   <AppointmentViewForDoctor/>
+<AppointmentViewForPatient/>
+
    </div>
    </div>
   )
