b0c763b5212c66bf86398bf29e434d6bedb7943b
ParamPS25 <bhavsarparam1940@gmail.com>
2025-02-18 21:28:34 +0530

    updated frontend url

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     // origin: process.env.FRONTEND_URL, // Allow only the specified frontend URL
     // origin: '*', // Allow any origin
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
