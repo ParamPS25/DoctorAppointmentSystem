79e0a5fb95b2825ceadb79f54dd37a0bbaddbaad
prince2004patel <princepatel30324@gmail.com>
2025-02-18 20:13:18 +0530

    upload artifacts

--- ml-backend/src/diabetes/data_ingestion.py
+++ ml-backend/src/diabetes/data_ingestion.py
@@ -16,8 +16,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../.
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts',"diabetes_train.csv")
-    test_data_path: str = os.path.join('artifacts',"diabetes_test.csv")
+    train_data_path: str = os.path.join('artifacts_dataset',"diabetes_train.csv")
+    test_data_path: str = os.path.join('artifacts_dataset',"diabetes_test.csv")
 
 class DataIngestion:
     def __init__(self):
